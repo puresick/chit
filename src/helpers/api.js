086aa851fc884fa5b2d2546c4254538bd50d9a28
puresick <2714266+puresick@users.noreply.github.com>
2019-12-29 18:09:04 +0100

    Add proxy ip

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,9 +1,9 @@
 const search = async params => {
 	const { query, type } = params
 	// const server = `localhost`
-	const server = `chitapi.hnnng.dev`
+	const server = `165.22.75.165/`
 	// const url = `https://${server}:3333/de/${type}/${query}`
-	const url = `https://${server}:3333/de/${type}/${query}`
+	const url = `http://${server}/de/${type}/${query}`
 	const response = await fetch(url)
 	const body = await response.json()
 	const result = body.response.docs.filter(
