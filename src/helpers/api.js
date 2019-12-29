69d59002658a9d1ac3229c11bb283d5492baca11
puresick <2714266+puresick@users.noreply.github.com>
2019-12-29 21:17:56 +0100

    Change api helper protocol to https

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,7 +3,7 @@ const search = async params => {
 	// const server = `localhost`
 	const server = `chitapi.hnnng.dev`
 	// const url = `https://${server}:3333/de/${type}/${query}`
-	const url = `http://${server}/de/${type}/${query}`
+	const url = `https://${server}/de/${type}/${query}`
 	const response = await fetch(url)
 	const body = await response.json()
 	const result = body.response.docs.filter(
