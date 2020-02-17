bca5c56af5f9a6eff5277030e355d6cb6f3809f6
Daniel Henning <daniel.henning@meso.design>
2020-02-17 14:49:05 +0100

    Change App.vue import using .vue extension preventing error on build time

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Vue from "vue"
 import router from "./router"
 import store from "./store"
 import i18n from "./i18n"
-import App from "./App"
+import App from "./App.vue"
 
 new Vue({
 	router,
