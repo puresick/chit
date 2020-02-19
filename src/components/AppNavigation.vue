a255e5298ba2cdbf1af28a87efc6faefd3a661a8
puresick <2714266+puresick@users.noreply.github.com>
2020-02-19 20:02:10 +0100

    Restyle AppNavigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,8 +1,8 @@
 <template>
-	<nav class="fixed bottom-0 mb-8 h-12 w-1/3 self-center flex justify-around items-center shadow text-gray-600 rounded-full overflow-hidden">
+	<nav class="fixed bottom-0 mb-8 h-12 w-1/3 self-center flex justify-around items-center bg-white text-gray-600 rounded-full overflow-hidden border-gray-300 border-2">
 		<router-link
-			class="bg-white h-full w-full flex flex-col items-center justify-center text-xs"
-			activeClass="text-white bg-blue-600 shadow-inner"
+			class="bg-white h-full w-full flex flex-col items-center justify-center text-xs rounded-full"
+			activeClass="text-white bg-blue-600"
 			v-for="(route, index) of routes"
 			:to="route.path"
 			exact
@@ -50,5 +50,9 @@
 		a:hover {
 			color: black !important;
 		}
+	}
+
+	nav {
+		box-shadow: 0 0.3rem 0 0.4rem #cbd5e0;
 	}
 </style>
