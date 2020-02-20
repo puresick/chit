b7007c209d994c77bee9ca685a6688ee421c74e3
puresick <2714266+puresick@users.noreply.github.com>
2020-02-20 10:42:32 +0100

    Clean up unused code

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,19 @@
 <template>
 	<article class="min-h-full flex flex-col bg-gray-100 max-w-screen-sm">
 		<router-view />
-		<!-- <AppButton                                -->
-		<!-- 	v-if="$route.path === '/saved'"          -->
-		<!-- 	float                                    -->
-		<!-- 	class="fixed bottom-0 mb-32 self-center" -->
-		<!-- 	:action="getSavedItemsFromLocalStorage"  -->
-		<!-- 	:loading="loading"                       -->
-		<!-- >                                         -->
-		<!-- 	<refresh-cw-icon />                      -->
-		<!-- </AppButton>                              -->
 		<AppNavigation />
 	</article>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex"
-import { RefreshCwIcon } from "vue-feather-icons"
 
-import AppButton from "./components/AppButton"
 import AppNavigation from "./components/AppNavigation"
 
 export default {
 	name: "App",
 	components: {
-		AppButton,
-		AppNavigation,
-		RefreshCwIcon
-	},
-	computed: {
-		...mapState([
-			"loading"
-		])
-	},
-	methods: {
-		...mapActions([
-			"getSavedItemsFromLocalStorage"
-		])
-	},
-	mounted() {
-		this.getSavedItemsFromLocalStorage()
+		AppNavigation
 	}
 }
 </script>
